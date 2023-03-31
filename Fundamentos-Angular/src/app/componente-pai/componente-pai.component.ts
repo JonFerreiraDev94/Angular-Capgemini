47bdc95064aa87805c36298addcbc45df0183109
JonFerreiraDev94 <98964725+JonFerreiraDev94@users.noreply.github.com>
2023-03-31 17:48:05 -0300

    Add files via upload

---
+++ Fundamentos-Angular/src/app/componente-pai/componente-pai.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-pai',
+  templateUrl: './componente-pai.component.html',
+  styleUrls: ['./componente-pai.component.css']
+})
+export class ComponentePaiComponent  {
+  sobrenome = "da Silva"
+
+  mostrarNomeCompleto(nomeCompleto: any){
+    alert(`o nome completo é: ${nomeCompleto}`);
+  }
+  
+  }
+
+
